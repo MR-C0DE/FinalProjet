3c4b4264a1f90022394f9efcefd3c0c95cab84c6
gakwa <82061740+gakwa@users.noreply.github.com>
2021-04-20 22:46:46 -0400

    Jean Marie
    Description

--- FinalProjet/Student.h
+++ FinalProjet/Student.h
@@ -1,35 +1,35 @@
 #pragma once
 #include <string>
+
+
 class Student
+
 {
 	
 public:
 	friend std::ostream& operator<< (std::ostream&, const Student&);
+
 	//Constructor
-
 	Student() = default;
-
 	Student(long, std::string, std::string);
+
 	//GETTERS
 	long getNumberStudent() const ;
 	std::string getFirstName() const ;
 	std::string getLastName() const ;
 
 	//SETTERS
-
 	void setNumberStudent(long);
 	void setFirstName(std::string);
 	void setLastName(std::string);
 
 	//Authors members functions
-
-	void confirmer(bool);
+	void afficher();
 	void afficherLesEtudiant();
 	void rechercheEtudiant(long) const;
-	void afficher();
+	void confirmer(bool);
 
 	//Destructor
-
 	virtual ~Student();
 private :
 	long numberStudent;
