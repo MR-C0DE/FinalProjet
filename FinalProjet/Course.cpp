649f5d44481ec14f9a871a1a835fe4e2b9b3f1d6
israelyav <82351602+israelyav@users.noreply.github.com>
2021-04-20 21:57:52 -0400

    Update Course.cpp

--- FinalProjet/Course.cpp
+++ FinalProjet/Course.cpp
@@ -8,7 +8,8 @@ using namespace std;
 
 //La difition d'un constructeur
 
-Course::Course(long CourseNumber, string code, string title) {
+Course::Course(long CourseNumber, string code, string title) 
+{
 
 	this->CourseNumber = CourseNumber;
 	this->code = code;
@@ -16,19 +17,23 @@ Course::Course(long CourseNumber, string code, string title) {
 }
 
 //La definition de GETTERS
-long Course::getCourseNumber() const noexcept {
+long Course::getCourseNumber() const noexcept 
+{
 	return this->CourseNumber;
 }
-string Course::getCode() const noexcept {
+string Course::getCode() const noexcept 
+{
 	return this->code;
 }
-string Course::getTitle() const noexcept {
+string Course::getTitle() const noexcept 
+{
 	return this->title;
 }
 
 //La definition de SETTERS
 
-void Course::setCourseNumber(long CourseNumber) {
+void Course::setCourseNumber(long CourseNumber) 
+{
 	this->CourseNumber = CourseNumber;
 }
 
@@ -43,15 +48,19 @@ void Course::setTitle(string title) {
 La fonction checkCoursNumber() verifie si le numero du cours existe 
 et génère un nouveau  si celui-ci existe  ou est deja utilisé
  */
-void Course::checkCoursNumber() {
+void Course::checkCoursNumber() 
+{
 	srand(time(0));
 	string cheminAcces{ "administration/cours/numeroCours.txt" };
 	ifstream fichier(cheminAcces.c_str());
 
-	if (fichier) {
+	if (fichier) 
+	{
 		string line; // capture de la ligne 
-		while (getline(fichier, line)) {
-			if (to_string(this->CourseNumber) == line) {
+		while (getline(fichier, line)) 
+		{
+			if (to_string(this->CourseNumber) == line) 
+			{
 				this->CourseNumber = 1000 + rand() % 5000; // La generation du numero du cours 
 				this->checkCoursNumber();
 			}
@@ -62,17 +71,20 @@ void Course::checkCoursNumber() {
 
 //	La fonction countCours() compte les cours disponible
  
-int Course::countCourse() {
+int Course::countCourse() 
+{
 
 	string cheminAcces = "administration/cours/numeroCours.txt";
 	ifstream fichier(cheminAcces.c_str());
 
 	int compteur = 0;
 
-	if (fichier) {
+	if (fichier) 
+	{
 		string line;// capture de la ligne
 
-		while (getline(fichier, line)) {
+		while (getline(fichier, line)) 
+		{
 
 			compteur++;
 		}
@@ -83,7 +95,8 @@ int Course::countCourse() {
 /*
 La fonction saveCourse() sauvegarde le cours apres sa creation
  */
-void Course::saveCourse(bool correct) {
+void Course::saveCourse(bool correct) 
+{
 	if (correct) {
 
 		string cheminAcces = "administration/cours/coursList.txt";
@@ -91,11 +104,13 @@ void Course::saveCourse(bool correct) {
 		ofstream fichier(cheminAcces.c_str(), ios::app);
 		ofstream fichier2(cheminAcces2.c_str(), ios::app);
 
-		if (fichier2) {
+		if (fichier2) 
+		{
 			fichier2 << this->getCourseNumber() << endl;
 		}
 		fichier2.close();
-		if (fichier) {
+		if (fichier)
+		 {
 			fichier << this->CourseNumber << " " << this->code << " " << this->title << endl;
 		}
 
@@ -103,14 +118,18 @@ void Course::saveCourse(bool correct) {
 	}
 }
 //La fonction afficherCours() permet d'afficher la totalités des cours disponible
-void Course::afficherCours() {
-	if (this->countCourse() != 0) {
+void Course::afficherCours() 
+{
+	if (this->countCourse() != 0) 
+	{
 		int compteur = 0;
 		string cheminAcces = "administration/cours/coursList.txt";
 		ifstream fichier(cheminAcces.c_str());
-		if (fichier) {
+		if (fichier) 
+		{
 			string line;
-			while (getline(fichier, line)) {
+			while (getline(fichier, line)) 
+			{
 				compteur++;
 				cout <<compteur<< ". " << line << endl;
 			}
